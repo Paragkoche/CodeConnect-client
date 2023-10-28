94b67ceccbecee25bb0a3342eb39c17e17461b88
Parag koche <paragkoche0@gmail.com>
2023-10-28 21:16:42 +0530

    Backend DONE

--- backend/src/router/Student.routes.ts
+++ backend/src/router/Student.routes.ts
@@ -1,5 +1,5 @@
 import { AnswerRepo } from "@/lib/repos";
-import { StudentObject } from "@/util/token-object";
+import { StudentObject, StudentReq } from "@/util/token-object";
 import { Router } from "express";
 
 const route = Router();
@@ -23,15 +23,51 @@ route.get("/get/:id", StudentObject, (req, res) => {
     });
   }
 });
-route.post("/submit-answer", StudentObject, (req, res) => {
+route.post("/submit-answer", StudentObject, (req: StudentReq, res) => {
   try {
-    //   const {ans,states,q} = req.body;
+    const { ans, states, q } = req.body;
+    if (!ans || !states || !q)
+      return res.status(403).json({
+        message: "Input required Answers Status and question ID",
+      });
+    const data = AnswerRepo.save(
+      AnswerRepo.create({
+        ans,
+        states,
+        q,
+        AnsBy: req.StudentObject,
+      })
+    );
+    return res.json({ data });
   } catch (e) {
     return res.status(500).json({
       message: e.toString(),
     });
   }
 });
-route.put("/update-answer/:id", StudentObject);
-route.delete("/delete-answer/:id", StudentObject);
+route.put("/update-answer/:id", StudentObject, (req: StudentReq, res) => {
+  try {
+    const { ans, states, q } = req.body;
+    const data = AnswerRepo.update(req.params.id, {
+      ans,
+      states,
+      q,
+    });
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
+route.delete("/delete-answer/:id", StudentObject, (req: StudentReq, res) => {
+  try {
+    const data = AnswerRepo.delete(req.params.id);
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
 export default route;
--- backend/src/router/Teacher.routes.ts
+++ backend/src/router/Teacher.routes.ts
@@ -1,4 +1,5 @@
-import { TeacherObject } from "@/util/token-object";
+import { CatalogRepo, QuestionRepo } from "@/lib/repos";
+import { TeacherObject, TeacherReq } from "@/util/token-object";
 import { Router } from "express";
 
 const route = Router();
@@ -7,14 +8,98 @@ const route = Router();
  * todo (update,crate,delete) question
  */
 
-route.get("/get/questions", TeacherObject);
-route.get("/get/catalog", TeacherObject);
+route.get("/get/questions", TeacherObject, (req: TeacherReq, res) => {
+  try {
+    const data = QuestionRepo.find({
+      where: {
+        createBy: {
+          id: req.teacherObject.id,
+        },
+      },
+    });
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
+route.get("/get/catalog", TeacherObject, (req: TeacherReq, res) => {
+  try {
+    const data = CatalogRepo.find();
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
 
-route.delete("/delete-question/:id", TeacherObject);
-route.post("/post-question", TeacherObject);
-route.put("/update-question/:id", TeacherObject);
+route.delete("/delete-question/:id", TeacherObject, (req, res) => {
+  try {
+    const data = QuestionRepo.delete(req.params.id);
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
+route.post("/post-question", TeacherObject, (req, res) => {
+  try {
+    const { q, testCase } = req.body;
+    const data = QuestionRepo.save(
+      QuestionRepo.create({
+        q,
+        testCase,
+      })
+    );
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
+route.put("/update-question/:id", TeacherObject, (req, res) => {
+  try {
+    const { q, testCase } = req.body;
+    const data = QuestionRepo.update(req.params.id, {
+      q,
+      testCase,
+    });
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
 
-route.delete("/delete-catalog/:id", TeacherObject);
-route.post("/post-catalog", TeacherObject);
+route.delete("/delete-catalog/:id", TeacherObject, (req, res) => {
+  try {
+    const data = CatalogRepo.delete(req.params.id);
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
+route.post("/post-catalog", TeacherObject, (req, res) => {
+  try {
+    const { name } = req.body;
+    const data = CatalogRepo.save(
+      CatalogRepo.create({
+        name,
+      })
+    );
+    return res.json({ data });
+  } catch (e) {
+    return res.status(500).json({
+      message: e.toString(),
+    });
+  }
+});
 
 export default route;
