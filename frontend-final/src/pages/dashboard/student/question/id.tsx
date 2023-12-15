a1fb740dc7cc894d58f10d26055891d007c49a5f
paragkoche <paragkoche0@gmail.com>
2023-12-15 17:28:24 +0530

    update

--- frontend-final/src/pages/dashboard/student/question/id.tsx
+++ frontend-final/src/pages/dashboard/student/question/id.tsx
@@ -13,11 +13,7 @@ import { useAppSelector } from "../../../../reducers/hook";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useParams, useNavigate } from "react-router-dom";
-import {
-  compile,
-  get_questions_s_id_api,
-  submit_answer_api,
-} from "../../../../api";
+import { get_questions_s_id_api, submit_answer_api } from "../../../../api";
 // import { Chart } from "@/mui/components/Chart";
 import Editor from "@monaco-editor/react";
 import Markdown from "react-markdown";
@@ -80,279 +76,324 @@ const languageOptions = [
     name: "JavaScript (Node.js 12.14.0)",
     label: "JavaScript (Node.js 12.14.0)",
     value: "javascript",
+    fileExtension: ".js",
   },
   {
     id: 45,
     name: "Assembly (NASM 2.14.02)",
     label: "Assembly (NASM 2.14.02)",
     value: "assembly",
+    fileExtension: ".asm",
   },
   {
     id: 46,
     name: "Bash (5.0.0)",
     label: "Bash (5.0.0)",
     value: "bash",
+    fileExtension: ".sh",
   },
   {
     id: 47,
     name: "Basic (FBC 1.07.1)",
     label: "Basic (FBC 1.07.1)",
     value: "basic",
+    fileExtension: ".bas",
   },
   {
     id: 75,
     name: "C (Clang 7.0.1)",
     label: "C (Clang 7.0.1)",
     value: "c",
+    fileExtension: ".c",
   },
   {
     id: 76,
     name: "C++ (Clang 7.0.1)",
     label: "C++ (Clang 7.0.1)",
     value: "cpp",
+    fileExtension: ".cpp",
   },
   {
     id: 48,
     name: "C (GCC 7.4.0)",
     label: "C (GCC 7.4.0)",
     value: "c",
+    fileExtension: ".c",
   },
   {
     id: 52,
     name: "C++ (GCC 7.4.0)",
     label: "C++ (GCC 7.4.0)",
     value: "cpp",
+    fileExtension: ".cpp",
   },
   {
     id: 49,
     name: "C (GCC 8.3.0)",
     label: "C (GCC 8.3.0)",
     value: "c",
+    fileExtension: ".c",
   },
   {
     id: 53,
     name: "C++ (GCC 8.3.0)",
     label: "C++ (GCC 8.3.0)",
     value: "cpp",
+    fileExtension: ".cpp",
   },
   {
     id: 50,
     name: "C (GCC 9.2.0)",
     label: "C (GCC 9.2.0)",
     value: "c",
+    fileExtension: ".c",
   },
   {
     id: 54,
     name: "C++ (GCC 9.2.0)",
     label: "C++ (GCC 9.2.0)",
     value: "cpp",
+    fileExtension: ".cpp",
   },
   {
     id: 86,
     name: "Clojure (1.10.1)",
     label: "Clojure (1.10.1)",
     value: "clojure",
+    fileExtension: ".clj",
   },
   {
     id: 51,
     name: "C# (Mono 6.6.0.161)",
     label: "C# (Mono 6.6.0.161)",
     value: "csharp",
+    fileExtension: ".cs",
   },
   {
     id: 77,
     name: "COBOL (GnuCOBOL 2.2)",
     label: "COBOL (GnuCOBOL 2.2)",
     value: "cobol",
+    fileExtension: ".cob",
   },
   {
     id: 55,
     name: "Common Lisp (SBCL 2.0.0)",
     label: "Common Lisp (SBCL 2.0.0)",
     value: "lisp",
+    fileExtension: ".lisp",
   },
   {
     id: 56,
     name: "D (DMD 2.089.1)",
     label: "D (DMD 2.089.1)",
     value: "d",
+    fileExtension: ".d",
   },
   {
     id: 57,
     name: "Elixir (1.9.4)",
     label: "Elixir (1.9.4)",
     value: "elixir",
+    fileExtension: ".ex",
   },
   {
     id: 58,
     name: "Erlang (OTP 22.2)",
     label: "Erlang (OTP 22.2)",
     value: "erlang",
+    fileExtension: ".erl",
   },
   {
     id: 44,
     label: "Executable",
     name: "Executable",
     value: "exe",
+    fileExtension: ".exe",
   },
   {
     id: 87,
     name: "F# (.NET Core SDK 3.1.202)",
     label: "F# (.NET Core SDK 3.1.202)",
     value: "fsharp",
+    fileExtension: ".fs",
   },
   {
     id: 59,
     name: "Fortran (GFortran 9.2.0)",
     label: "Fortran (GFortran 9.2.0)",
     value: "fortran",
+    fileExtension: ".f",
   },
   {
     id: 60,
     name: "Go (1.13.5)",
     label: "Go (1.13.5)",
     value: "go",
+    fileExtension: ".go",
   },
   {
     id: 88,
     name: "Groovy (3.0.3)",
     label: "Groovy (3.0.3)",
     value: "groovy",
+    fileExtension: ".groovy",
   },
   {
     id: 61,
     name: "Haskell (GHC 8.8.1)",
     label: "Haskell (GHC 8.8.1)",
     value: "haskell",
+    fileExtension: ".hs",
   },
   {
     id: 62,
     name: "Java (OpenJDK 13.0.1)",
     label: "Java (OpenJDK 13.0.1)",
     value: "java",
+    fileExtension: ".java",
   },
-
   {
     id: 78,
     name: "Kotlin (1.3.70)",
     label: "Kotlin (1.3.70)",
     value: "kotlin",
+    fileExtension: ".kt",
   },
   {
     id: 64,
     name: "Lua (5.3.5)",
     label: "Lua (5.3.5)",
     value: "lua",
+    fileExtension: ".lua",
   },
-
   {
     id: 79,
     name: "Objective-C (Clang 7.0.1)",
     label: "Objective-C (Clang 7.0.1)",
     value: "objectivec",
+    fileExtension: ".m",
   },
   {
     id: 65,
     name: "OCaml (4.09.0)",
     label: "OCaml (4.09.0)",
     value: "ocaml",
+    fileExtension: ".ml",
   },
   {
     id: 66,
     name: "Octave (5.1.0)",
     label: "Octave (5.1.0)",
     value: "octave",
+    fileExtension: ".m",
   },
   {
     id: 67,
     name: "Pascal (FPC 3.0.4)",
     label: "Pascal (FPC 3.0.4)",
     value: "pascal",
+    fileExtension: ".pas",
   },
   {
     id: 85,
     name: "Perl (5.28.1)",
     label: "Perl (5.28.1)",
     value: "perl",
+    fileExtension: ".pl",
   },
   {
     id: 68,
     name: "PHP (7.4.1)",
     label: "PHP (7.4.1)",
     value: "php",
+    fileExtension: ".php",
   },
   {
     id: 43,
     label: "Plain Text",
     name: "Plain Text",
     value: "text",
+    fileExtension: ".txt",
   },
   {
     id: 69,
     name: "Prolog (GNU Prolog 1.4.5)",
     label: "Prolog (GNU Prolog 1.4.5)",
     value: "prolog",
+    fileExtension: ".pl",
   },
   {
     id: 70,
     name: "Python (2.7.17)",
     label: "Python (2.7.17)",
     value: "python",
+    fileExtension: ".py",
   },
   {
     id: 71,
     name: "Python (3.8.1)",
     label: "Python (3.8.1)",
     value: "python",
+    fileExtension: ".py",
   },
   {
     id: 80,
     name: "R (4.0.0)",
     label: "R (4.0.0)",
     value: "r",
+    fileExtension: ".r",
   },
   {
     id: 72,
     name: "Ruby (2.7.0)",
     label: "Ruby (2.7.0)",
     value: "ruby",
+    fileExtension: ".rb",
   },
   {
     id: 73,
     name: "Rust (1.40.0)",
     label: "Rust (1.40.0)",
     value: "rust",
+    fileExtension: ".rs",
   },
   {
     id: 81,
     name: "Scala (2.13.2)",
     label: "Scala (2.13.2)",
     value: "scala",
+    fileExtension: ".scala",
   },
   {
     id: 82,
     name: "SQL (SQLite 3.27.2)",
     label: "SQL (SQLite 3.27.2)",
     value: "sql",
+    fileExtension: ".sql",
   },
   {
     id: 83,
     name: "Swift (5.2.3)",
     label: "Swift (5.2.3)",
     value: "swift",
+    fileExtension: ".swift",
   },
   {
     id: 74,
     name: "TypeScript (3.7.4)",
     label: "TypeScript (3.7.4)",
     value: "typescript",
+    fileExtension: ".ts",
   },
   {
     id: 84,
     name: "Visual Basic.Net (vbnc 0.0.0.5943)",
     label: "Visual Basic.Net (vbnc 0.0.0.5943)",
     value: "vbnet",
+    fileExtension: ".vb",
   },
+  // ... (add more languages as needed)
 ];
 
 function PageHeader() {
@@ -409,40 +450,40 @@ function PageHeader() {
   const [code, setCode] = React.useState(
     `function add(a, b) {\n  return a + b;\n}`
   );
-  const [output, setOut] = React.useState<any>();
-  const checkStatus = async (token: string) => {
-    const options = {
-      Method: "GET",
-      url: "https://judge0-ce.p.rapidapi.com/submissions" + "/" + token,
-      params: { base64_encoded: "true", fields: "*" },
-      headers: {
-        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-        "X-RapidAPI-Key": "699fc58353msh5a6c3e4f14e91f9p129e98jsnec1f56e4da5d",
-      },
-    };
-    try {
-      const response = await axios.request(options);
-      const statusId = response.data.status?.id;
+  const [output, setOut] = React.useState<string>();
+  // const checkStatus = async (token: string) => {
+  //   const options = {
+  //     Method: "GET",
+  //     url: "https://judge0-ce.p.rapidapi.com/submissions" + "/" + token,
+  //     params: { base64_encoded: "true", fields: "*" },
+  //     headers: {
+  //       "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+  //       "X-RapidAPI-Key": "699fc58353msh5a6c3e4f14e91f9p129e98jsnec1f56e4da5d",
+  //     },
+  //   };
+  //   try {
+  //     const response = await axios.request(options);
+  //     const statusId = response.data.status?.id;
 
-      // Processed - we have a result
-      if (statusId === 1 || statusId === 2) {
-        // still processing
-        setTimeout(() => {
-          checkStatus(token);
-        }, 2000);
-        return;
-      } else {
-        setProcessing(false);
-        setOut(response.data);
-        toast.success(`Compiled Successfully!`);
-        console.log("response.data", response.data);
-        return;
-      }
-    } catch (err) {
-      console.log("err", err);
-      setProcessing(false);
-    }
-  };
+  //     // Processed - we have a result
+  //     if (statusId === 1 || statusId === 2) {
+  //       // still processing
+  //       setTimeout(() => {
+  //         checkStatus(token);
+  //       }, 2000);
+  //       return;
+  //     } else {
+  //       setProcessing(false);
+  //       setOut(response.data);
+  //       toast.success(`Compiled Successfully!`);
+  //       console.log("response.data", response.data);
+  //       return;
+  //     }
+  //   } catch (err) {
+  //     console.log("err", err);
+  //     setProcessing(false);
+  //   }
+  // };
   console.log(_theme);
 
   const [process, setProcessing] = React.useState(false);
@@ -461,11 +502,7 @@ function PageHeader() {
                     data.testCase +
                     ""}
                 </Markdown>
-                {output && (
-                  <Markdown>
-                    {"# output\n" + (atob(output.stdout || "") || "")}
-                  </Markdown>
-                )}
+                {output && <Markdown>{"# output\n" + (output || "")}</Markdown>}
               </Box>
               <Box width={"50%"}>
                 <Box width={"100%"} gap={2} mx={1} my={2}>
@@ -520,16 +557,30 @@ function PageHeader() {
             </Box>
             <Button
               onClick={() => {
-                const matches = (data.testCase as string).match(
-                  /\b\d+\b/g
-                ) as RegExpMatchArray;
-
-                const inputValues = matches.map(Number);
-                compile(code, JSON.parse(language).id, inputValues.toString())
-                  .then((data) => {
-                    setProcessing(true);
-                    checkStatus(data.data.token);
-                    // console.log(data.data);
+                const options = {
+                  method: "POST",
+                  url: "https://onecompiler-apis.p.rapidapi.com/api/v1/run",
+                  headers: {
+                    "content-type": "application/json",
+                    "X-RapidAPI-Key":
+                      "1b9bd6cec6msh5dcd96b4ee2fa71p175f6djsn62cff0ed1ab7",
+                    "X-RapidAPI-Host": "onecompiler-apis.p.rapidapi.com",
+                  },
+                  data: {
+                    language: JSON.parse(language).value,
+                    files: [
+                      {
+                        name: "filename" + JSON.parse(language).fileExtension,
+                        content: code,
+                      },
+                    ],
+                  },
+                };
+                setProcessing(true);
+                axios
+                  .request(options)
+                  .then(({ data }) => {
+                    setOut(data.stdout);
                   })
                   .catch((err: AxiosError) => {
                     if (err.response?.status === 429) {
@@ -539,6 +590,9 @@ function PageHeader() {
                         `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`
                       );
                     }
+                  })
+                  .finally(() => {
+                    setProcessing(false);
                   });
               }}
               disabled={process}
